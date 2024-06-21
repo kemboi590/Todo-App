5825ad20a07f3b54d37f7bd7564166eb9f61af77
kemboi590 <bkemboi590@gmail.com>
2024-06-21 16:48:32 +0300

    added more styles

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -41,13 +41,15 @@ export const TodoItem = ({ id, text, completed }: TodoItemProps) => {
           <button type="submit">Save</button>
         </form>
       ) : (  // if the user is not editing the todo, display the todo	
-        <div>
+        <div className='todo-item-todo'>
           <input type="checkbox" checked={completed} onChange={handleToggle} />
           <span style={{ textDecoration: completed ? 'line-through' : undefined }}>
             {text}
           </span>
-          <button onClick={() => setIsEditing(true)}>Edit</button>
-          <button onClick={handleDelete}>Delete</button>
+          <div className="buttons">
+          <button onClick={() => setIsEditing(true)} className='edit-btn'>Edit</button>
+          <button onClick={handleDelete} className='delete-btn'>Delete</button>
+          </div>
         </div>
       )}
     </div>
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,14 +1,14 @@
-import  { useContext } from 'react';
+import { useContext } from 'react';
 import { TodoContext } from '../../Context/TodoContext';
 import { TodoItem } from '../TodoItem/TodoItem';
 
-export const TodoList = () => { //  display the list of todos
-    const { state } = useContext(TodoContext)!;
-    return (
-        <div>
-          {state.map(todo => (
-            <TodoItem key={todo.id} {...todo} />
-          ))}
-        </div>
-      );
-    };
+export const TodoList = () => {
+  const { state } = useContext(TodoContext)!;
+  return (
+    <div className="todo-list"> {/* Add classname to the div */}
+      {state.map(todo => (
+        <TodoItem key={todo.id} {...todo} />
+      ))}
+    </div>
+  );
+};
